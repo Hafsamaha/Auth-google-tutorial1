2b585211f8d59bf6b30c612171dc89028a66e851
Hafsamaha <sidi.lotfi@docc.techstarter.de>
2024-02-08 18:05:45 +0100

    Hausaufgabe Auth

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>Hier könnte dein Login-Formular stehen.</p>
+    </div>
+  );
+}
+
+export default Login;
